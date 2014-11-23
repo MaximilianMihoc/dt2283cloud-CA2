600a8bd5808498b7d280ffb1f6ae0e9c0065fa1f
MaximilianMihoc <Maximilian@Mihoc>
2014-11-23 23:52:46 +0000

    create admin and member lists and created the simple way of saying if a user is admin, member or guest into this App

--- PictureBox/src/ie/dit/mihoc/maximilian/PictureBoxServlet.java
+++ PictureBox/src/ie/dit/mihoc/maximilian/PictureBoxServlet.java
@@ -1,13 +1,70 @@
 package ie.dit.mihoc.maximilian;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.*;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class PictureBoxServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		UserService userService = UserServiceFactory.getUserService(); 
+		Principal myPrincipal = req.getUserPrincipal();
+		String emailAddress = null;
+		
+		String thisURL = req.getRequestURI(); 
+		String loginURL = userService.createLoginURL(thisURL);
+		String logoutURL = userService.createLogoutURL(thisURL);
+		String registerURL = "/register.jsp";
+		
+		//define admins List 
+		List<String> admins = new ArrayList<String>();
+		admins.add(("MaximiliaMihoc@gmail.com").toLowerCase());
+		admins.add(("mark.deegan@dit.ie").toLowerCase());
+		
+		//define members List
+		List<String> members = new ArrayList<String>();
+		members.add(("member@mail.com").toLowerCase());
+		
+		
+		resp.setContentType("text/html");
+		resp.getWriter().println("<h1>Welcome to Picture Box Web Application</h1>");
+		
+		if(myPrincipal == null) 
+		{
+			resp.getWriter().println("<p>You are Not Logged In</p>");
+			resp.getWriter().println("<p>You can <a href=\"" + loginURL + "\">sign in here</a>.</p>");
+			resp.getWriter().println("<p>Not registred You can <a href=\"" + registerURL + "\">Register here</a>.</p>");
+		} 
+		
+		if(myPrincipal != null) 
+		{
+			emailAddress = myPrincipal.getName();
+			if(admins.contains(emailAddress.toLowerCase()))
+			{
+				resp.getWriter().println("<h3>Admin</h3>");
+				resp.getWriter().println("<p>You are Logged in as (email): " + emailAddress + "</p>");
+				resp.getWriter().println("<p>You can <a href=\"" + logoutURL + "\">sign out</a>.</p>");
+			}
+			else if(members.contains(emailAddress.toLowerCase()))
+			{
+				resp.getWriter().println("<h3>Member</h3>");
+				resp.getWriter().println("<p>You are Logged in as (email): " + emailAddress + "</p>");
+				resp.getWriter().println("<p>You can <a href=\"" + logoutURL + "\">sign out</a>.</p>");
+			}
+			else 
+			{
+				resp.getWriter().println("<h3>Guest</h3>");
+				resp.getWriter().println("<p>You are not Logged in You can <a href=\"" + loginURL + "\">sign in here</a>");
+			}
+		}
 	}
 }
---
+++ PictureBox/war/uploadSuccess.jsp
@@ -0,0 +1,15 @@
+<%@ page 
+import="com.google.appengine.api.blobstore.BlobstoreServiceFactory" %>
+<%@ page import="com.google.appengine.api.blobstore.BlobstoreService" %>
+<%BlobstoreService blobstoreService = 
+BlobstoreServiceFactory.getBlobstoreService(); %>
+
+<html>
+ <head>
+ <title>Flop Box</title>
+ </head>
+ <body>
+ 	<h2>File Uploaded Successfully</h2>
+ 	<a href="index.jsp">Back</a>
+ </body>
+</html>
